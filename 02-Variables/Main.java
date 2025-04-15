c8ad50c871b5223a9daea6a274ff2470b80d73ed
Inderpaul Singh <botakpai@hotmail.com>
2025-04-15 17:24:36 +0000

    updated 01/02/03

--- 02-Variables/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    // All Java program start from a 'main' function
-    public static void main(String[] args) {
-
-        // console.log
-        System.out.println("Hello World");
-    }
-}
