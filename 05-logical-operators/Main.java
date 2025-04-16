c10e5eedcc16b87ab7e5a7fabf2c10c8cbbf4e38
Inderpaul Singh <botakpai@hotmail.com>
2025-04-16 15:18:35 +0000

    updated 04/05/06

--- 05-logical-operators/Main.java
+++ 05-logical-operators/Main.java
@@ -1,34 +1,44 @@
-// package 05-logical-operators;
 
 public class Main {
-    public static void main(String[] args){
-        // Comparison and Logical Operators
+    public static void main(String[] args) {
+        // Comparsion and Logical operators
 
         // Comparison operators work the same as in JavaScript
         System.out.println(5 > 10);
         System.out.println(5 <= 10);
-        // There is only double equal in Java
+        // There's only double equal in Java
         System.out.println(3 == 3);
 
-        // Logical Operators
-        boolean a = true; 
+        // Logical operators
+        boolean a = true;
         boolean b = false;
 
-        // TRUE aoperator is &&
-        // Only true && true => true
+        // TRUE operator is &&
+        // Only true && true ==> true
         // Anything else (true && false, or false && true) will be false
         System.out.println("a && b =>" + (a && b));
 
         // OR operator is || (double-pipe character)
         // As long as one side of the OR is true, the entire expression is evaluated
         // to be true
-        System.out.println("a || b =>" + (a || b));
+        System.out.println("a || b => " + (a || b));
 
-        if (a == true){
+        if (a == true) {
             System.out.println("A is true");
         } else {
-            
+            System.out.println("A is false");
         }
+
+        double score = 82;
+        if (score >= 80) {
+            System.out.println("A");
+        } else if (score >= 75) {
+            System.out.println("B+");
+        } else if (score >= 70) {
+            System.out.println("B");
+        } else {
+            System.out.println("Score less than B");
+        }
+
     }
-    
 }
